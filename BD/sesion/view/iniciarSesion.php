f306453dfc2a657da39753728fac9ede4a28ee8e
AbiezerRamirez <abiezerj.ramceb@educa.jcyl.es>
2021-11-17 19:22:22 +0100

    Sesion

---
+++ BD/sesion/view/iniciarSesion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesion</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="user">Usuario</label>
+        <input type="text" name="user" id="user"><br>
+        <label for="pass">Contraseña</label>
+        <input type="password" name="pass" id="pass"><br>
+        <input type="submit" value="Iniciar Sesion">
+    </form>
+</body>
+</html>
