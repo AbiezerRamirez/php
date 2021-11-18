fb2cb3d4e172abdb755c25e6b9bab81cbbaf77ca
AbiezerRamirez <abiezerj.ramceb@educa.jcyl.es>
2021-11-18 23:27:25 +0100

    hola

---
+++ test-desplige/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+    <form action="">
+        <input type="button" value="pagina 1" formaction="pag1.php" formaction="get">
+
+    </form>
+    <!-- <button type="submit"><a href="pag1.php">Pa</a></button> -->
+</body>
+</html>
