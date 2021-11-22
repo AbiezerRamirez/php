e951bc083c8c2be3a49731425a8fd78182d0a050
AbiezerRamirez <abiezerj.ramceb@educa.jcyl.es>
2021-11-22 17:19:37 +0100

    consultas

---
+++ mysqlConsultas.php
@@ -0,0 +1,18 @@
+<?php
+// INSERTAR 
+
+// INSERT INTO table_name (column1, column2, column3, ...)
+// VALUES (value1, value2, value3, ...);
+
+// - - - - - - - - -
+
+// ACTUALIZAR 
+
+// UPDATE tabla SET campo = ‘valor’ WHERE condición
+// UPDATE tabla SET campo = ‘valor’, campo2 = ‘valor2’ WHERE condición
+
+// - - - - - - - - -
+
+// ELIMINAR
+
+// DELETE FROM nombre_tabla WHERE condicion
