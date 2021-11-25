f55fd1dac787b6f8324fb718206378f9b529ebeb
AbiezerRamirez <abiezerj.ramceb@educa.jcyl.es>
2021-11-25 20:58:50 +0100

    mejorando el login - Modelo

--- BD/sesion/model/bd.php
+++ BD/sesion/model/bd.php
@@ -9,9 +9,9 @@ function validar($user, $password, $conexion)
         $stm = $conexion->prepare('select * from usuarios where user = :p1');
         $stm->bindParam(':p1', $user);
         $stm->execute();
-
+        
         $array = $stm->fetch(PDO::FETCH_ASSOC);
-
+        
         if ($array > 0) {
             if ($password == $array['password']) {
                 $_SESSION['user'] = $user;
@@ -22,5 +22,55 @@ function validar($user, $password, $conexion)
         
     } catch (Exception $e) {
         die($e->getMessage());
+    }
+}
+
+class GBD {
+    private $conexion;
+    
+    public function __construct($name)
+    {
+        try {
+            $conexion = new PDO("mysql:dbname=$name;host=127.0.0.1", 'root', '');
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+        
+    }
+    
+    public function executeQuery($query)
+    {
+        try {
+            $stm = $this->conexion->prepare($query);
+            $stm->execute();
+            // return true;??? PROBAR
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+        
+    }
+    
+    public function add()
+    {
+        
+    }
+
+    public function getElementsArray($query)
+    {
+        try {
+            $stm = $this->conexion->prepare($query);
+            $stm->execute();
+            return $stm->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+    
+
+
+    public function disconect()
+    {
+        $this->conexion = null;
     }
 }
